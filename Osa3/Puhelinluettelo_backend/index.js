fc31f0c77d94190a1be090c2c16a6d00c4d2d1ae
SamuliVen <64473401+SamuliVen@users.noreply.github.com>
2020-06-11 13:12:21 +0300

    Add files via upload

---
+++ Osa3/Puhelinluettelo_backend/index.js
@@ -0,0 +1,100 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+app.use(morgan('tiny'))
+
+// app.use(express.json())
+
+let persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+    },
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+    },
+    {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+    }
+]
+
+const generateId = () => {
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(p => p.id))
+        : 0
+    return maxId + 1
+}
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    response.send('<p> Phonebook has info for ' + (generateId() - 1) + ' people </p> <p>' + new Date() + '</p>')
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+
+    if (!body.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: Math.floor(Math.random() * Math.floor(1000)),
+    }
+
+    if (persons.map(p => p.name).includes(body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique' 
+        })
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
